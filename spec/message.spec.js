2e93b6c7059291c58bae93171cb89c4f1c21fdf6
kcpage816 <kierstendotpaige@gmail.com>
2024-02-25 04:12:11 -0600

    rover parts 1-4 completed

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -1,9 +1,20 @@
 const Message = require('../message.js');
 const Command = require('../command.js');
 
-// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
-
 describe("Message class", function() {
+
+    it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+        expect( function() { new Message();}).toThrow(new Error('Name is required.'));
+      });
+
+    it("constructor sets name", function() {
+        let messageNameTest = new Message('Test Message', 'commands');
+        expect(messageNameTest.name).toEqual('Test Message');
+      });
+
+    it("contains a commands array passed into the constructor as the 2nd argument", function() {
+        let messageCommandsTest = new Message ('name', ['commands'])
+        expect(messageCommandsTest.commands).toEqual(['commands']);
+    });
 
 });
